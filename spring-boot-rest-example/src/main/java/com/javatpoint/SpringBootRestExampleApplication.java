44d26b7f5cf7ed788975a11bd93358a0a0479ce3
JOSUERV99 <josuerojasvega@gmail.com>
2023-06-02 20:30:16 -0600

    CX: updating example project because of library changes

--- spring-boot-rest-example/src/main/java/com/javatpoint/SpringBootRestExampleApplication.java
+++ spring-boot-rest-example/src/main/java/com/javatpoint/SpringBootRestExampleApplication.java
@@ -3,10 +3,14 @@ package com.javatpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringBootRestExampleApplication
+ * @author josuerv99
+ */
 @SpringBootApplication
 public class SpringBootRestExampleApplication
 {
-public static void main(String[] args) 
+    public static void main(String[] args)
 {
 SpringApplication.run(SpringBootRestExampleApplication.class, args);
 }
